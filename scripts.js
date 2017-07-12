893f40844d08350764ebd983e02bce434ae2d9e7
Joseph Hill <hilljoseph163@gmail.com>
2017-07-12 14:00:29 -0500

    More changes

--- scripts.js
+++ scripts.js
@@ -1,3 +1,13 @@
 function myFunction() {
-    alert("Hello! I am an alert box!");
+    alert("Your form has been submitted!");
+}
+
+function submitContractForm() {
+    var x = document.forms["contactForm"]["email"].value;
+    if (x == "") {
+        alert("Email must be filled out");
+        return false;
+    } else {
+        alert('Thank you for submitting the form');
+    }  
 }
